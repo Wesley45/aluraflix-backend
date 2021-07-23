60e6f2c229f7839d5f5fa2db5a561ec82bf85c07
Wesley455 <wesley@drterapia.com>
2021-07-22 22:34:29 -0300

    finalizado teste de integração de videos

--- tests/VideoTest.php
+++ tests/VideoTest.php
@@ -6,27 +6,91 @@ class VideoTest extends TestCase
 {
     use DatabaseMigrations;
 
+    private array $data = [
+        'title' => 'Diferença entre Back-End e Front-End com Mario Souto',
+        'description' => 'Qual a diferença entre Back-end e Front-end? É possível se especializar nas duas áreas? Como é a rotina desses profissionais e com quais outras áreas da tecnologia acabam se relacionando?',
+        'url' => 'https://www.youtube.com/watch?v=xrRy3RI3HE4&t=766s'
+    ];
+    private $responseData;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->responseData = $this->json(
+            'POST',
+            'api/videos',
+            $this->data,
+            ['Accept' => 'application/json']);
+    }
+
     public function testShouldBeAbleToCreateAVideo()
     {
-        $data = [
-            'title' => 'Diferença entre Back-End e Front-End com Mario Souto',
-            'description' => 'Qual a diferença entre Back-end e Front-end? É possível se especializar nas duas áreas? Como é a rotina desses profissionais e com quais outras áreas da tecnologia acabam se relacionando?',
-            'url' => 'https://www.youtube.com/watch?v=xrRy3RI3HE4&t=766s'
-        ];
-
-        $response = $this->call('POST', '/api/videos', $data);
-
-        $this->assertEquals(201, $response->status());
+        $this->responseData->seeStatusCode(201)
+            ->seeJsonStructure([
+                'title',
+                'description',
+                'url',
+            ])
+            ->seeJsonEquals([
+                'id' => 1,
+                'title' => $this->data['title'],
+                'description' => $this->data['description'],
+                'url' => $this->data['url'],
+            ]);
     }
 
     public function testShouldBeAbleToGeAllVideo()
     {
-        $response = $this->call('GET', '/api/videos');
-        $videos = json_decode($response->content(), true);
+        $response = $this->get('api/videos')
+            ->seeStatusCode(200)
+            ->seeJsonStructure(
+                ['data' =>
+                    [
+                        0 => [
+                            'id',
+                            'title',
+                            'description',
+                            'url',
+                        ]
+                    ]
+                ]
+            );
 
-        echo "<pre>";
-        print_r($videos);
-        echo "<pre>";
-        $this->assertEquals(200, $response->status());
+        $video = json_decode($response->response->content());
+
+        $this->assertEquals(1, $video->data[0]->id);
+        $this->assertEquals($this->data['title'], $video->data[0]->title);
+        $this->assertEquals($this->data['description'], $video->data[0]->description);
+        $this->assertEquals($this->data['url'], $video->data[0]->url);
+    }
+
+    public function testShouldBeAbleToUpdateVideo()
+    {
+        $updateData = [
+            'title' => 'Diferença entre Back-End e Front-End com Mario Santos',
+            'description' => 'Qual a diferença entre Back-end e Front-end? É possível se especializar nas duas áreas? Como é a rotina desses profissionais e com quais outras áreas da tecnologia acabam se relacionando?',
+            'url' => 'https://www.youtube.com/watch?v=xrRy3RI3HE4&t=766s'
+        ];
+
+        $this->json('PUT', 'api/videos/1', $updateData, ['Accept' => 'application/json'])
+            ->seeStatusCode(200)
+            ->seeJsonStructure([
+                'id',
+                'title',
+                'description',
+                'url',
+            ])
+            ->seeJsonEquals([
+                'id' => 1,
+                'title' => $updateData['title'],
+                'description' => $updateData['description'],
+                'url' => $updateData['url'],
+            ]);
+    }
+
+    public function testShouldBeAbleToDeleteAVideo()
+    {
+        $this->json('DELETE', 'api/videos/1', [], ['Accept' => 'application/json'])
+            ->seeStatusCode(204);
     }
 }
